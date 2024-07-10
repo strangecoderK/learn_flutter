6e3a5b30feb3c87ed5a46e8b7943a02fda4134aa
Kimdahee <dh980502@gmail.com>
2024-07-11 00:35:22 +0900

    feat : fruit card widget 작성

--- lib/01_widget_rule/presentation/main_screen.dart
+++ lib/01_widget_rule/presentation/main_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/01_widget_rule/data/model/fruit.dart';
-import 'package:learn_flutter/01_widget_rule/presentation/componant/fruit_cart.dart';
+import 'package:learn_flutter/01_widget_rule/presentation/componant/fruit_card.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -13,17 +13,17 @@ class _MainScreenState extends State<MainScreen> {
   final fruit = const Fruit(
       image:
           'https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg',
-      name: 'tomato',
+      name: 'Tomatos',
       weight: '500');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('제목'),
+        title: const Text('Widget Rule'),
       ),
       body: Column(
-        children: [FruitCart(fruit: fruit)],
+        children: [FruitCard(fruit: fruit)],
       ),
     );
   }
