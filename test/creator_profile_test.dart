975edcedbd8c269892e6d383d28a38b15b3e28be
Kimdahee <dh980502@gmail.com>
2024-08-09 18:29:49 +0900

    chore : gitignore 에 freezed, g.dart 추가

--- test/creator_profile_test.dart
+++ test/creator_profile_test.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/material.dart';
+
 import 'package:flutter_test/flutter_test.dart';
-import 'package:learn_flutter/01_widget_rule/presentation/main_screen.dart';
+
+import 'package:learn_flutter/main.dart';
 
 void main() {
   testWidgets('creator dialog test', (tester) async {
-    await tester.pumpWidget(const MaterialApp(home: MainScreen()));
+    await tester.pumpWidget(const MyApp());
     // expect(find.text('follow'), findsOneWidget);
   });
 }
